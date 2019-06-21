4f68b4a1b02d6631146c3c54fab1c735cc26d418
Matthew Gleich <matthewgleich@gmail.com>
2019-06-20 23:31:33 -0400

    Working version of upload_score_data

--- database_functions.py
+++ database_functions.py
@@ -1,8 +1,9 @@
 import firebase_admin
 from firebase_admin import credentials
+from firebase_admin import firestore
 
 
-def upload(home_score, away_score, game_time, period, away_team_name, sport, event_start, event_end):
+def upload_score_data(home_score, away_score, game_time, period, away_team_name, sport, event_start, event_end, varsity, gender):
     """
     Uploads data to the database
     :param home_score: GHS score (int)
@@ -13,8 +14,35 @@ def upload(home_score, away_score, game_time, period, away_team_name, sport, eve
     :param sport: Sport Name (str)
     :param event_start: Event start time (str)
     :param event_end: Event start time (str)
+    :param varsity: Varsity or Junior Varsity (Boolean)
+    :param gender: Either "m" or "w"
     :return: none
     """
-    cred = credentials.Certificate("path/to/serviceAccountKey.json")
+    # Data manipulations:
+    if varsity:
+        collection_name = "varsity-scores"
+    elif not varsity:
+        collection_name = "jv-scores"
+    document_name = gender.upper() + "-" + sport.lower()
+
+    # Firestore interactions:
+    cred = credentials.Certificate("firestore_creds.json")
     firebase_admin.initialize_app(cred)
-    db = 
+    db = firestore.client()
+    doc_ref = db.collection(collection_name).document(document_name)
+    doc_ref.set({
+        "period": period,
+        "away_score": away_score,
+        "currently_playing": True,
+        "game_time": game_time,
+        "home_score": home_score,
+        "away_team_name": away_team_name,
+
+    })
+
+
+# Testing:
+# upload_score_data(4, 1, "00:02:45", 2, "Jaguars", "soccer", "Now", "Later", True, "m")
+
+
+
