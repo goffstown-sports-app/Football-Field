fd9885d1b3aa75e7d08bcc26c2e8e46629856e31
Matthew Gleich <matthewgleich@gmail.com>
2019-06-30 15:40:38 -0400

    Fix datetime import

--- database_functions.py
+++ database_functions.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 import utility_functions as UF
-from datatime import datetime
+from datetime import datetime
 import json
 
 
